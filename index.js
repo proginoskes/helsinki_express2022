30ea19fc5de499377bc6283c2dde80fbf4156eff
proginoskes <56172200+proginoskes@users.noreply.github.com>
2022-06-02 13:24:25 -0400

    corrects lint errors

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 require('dotenv').config()
 
-const { response } = require('express');
-const express = require('express');
-const morgan = require('morgan');
-const cors = require('cors');
-const app = express();
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const app = express()
 
-const Person = require('./models/person');
+const Person = require('./models/person')
 
-morgan.token('response-body', function getId (req, res) {
+morgan.token('response-body', function getId (req) {
     return JSON.stringify(req.body)
 })
-
 
 // json parser for POST
 app.use(express.json())
@@ -23,7 +21,7 @@ app.use(express.static('build'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :response-body'))
 
 // cors for cross origin resource sharing
-app.use(cors());
+app.use(cors())
 
 //app.use(requestLogger)
 
@@ -32,7 +30,7 @@ const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' })
 }
   
-  // handler of requests with unknown endpoint
+// handler of requests with unknown endpoint
 
 // error handling middleware
 const errorHandler = (error, request, response, next) => {
@@ -46,26 +44,28 @@ const errorHandler = (error, request, response, next) => {
     }
   
     next(error)
-  }
+}
 
 
 // REST funcs
 app.get('/', (request, response) => {
     response.send('<h1>Welcome to Phonebook Server</h1>')
 })
-app.get('/info', (request, response) => {
-    Person.find({}).then(persons =>{
-        response.send(
-            `
-            <p>Phonebook has info for ${persons.length} people</p>
-            <p>${Date()}</p>
-            `
-        )
-    })
-    .catch(error=>next(error));
+app.get('/info', (request, response, next) => {
+    Person
+        .find({})
+        .then(persons =>{
+            response.send(
+                `
+                <p>Phonebook has info for ${persons.length} people</p>
+                <p>${Date()}</p>
+                `
+            )
+        })
+        .catch(error=>next(error))
 })
 
-app.get('/api/persons', (request, response) => {
+app.get('/api/persons', (request, response, next) => {
     Person.find({}).then(persons => {
         response.json(persons)
     }).catch(error=>next(error))
@@ -75,30 +75,30 @@ app.get('/api/persons/:id', (request,response, next)=>{
 
     Person.findById(request.params.id).then(person => {
         if(person){
-            response.json(person);
+            response.json(person)
         }else{
-            response.status(404).end();
+            response.status(404).end()
         }
-    }).catch((error)=>next(error));
+    }).catch((error)=>next(error))
 
 })
 
 app.delete('/api/persons/:id', (request, response, next)=>{
     Person.findByIdAndRemove(request.params.id)
-        .then(result => {
-            response.status(204).end();
+        .then(() => {
+            response.status(204).end()
         })
-        .catch(error=>next(error));
+        .catch(error=>next(error))
 })
 
 // add a number
 app.post('/api/persons', (request, response, next)=>{
-    const body = request.body;
+    const body = request.body
 
     const person = new Person({
         name: body.name,
         number: body.number
-    });
+    })
 
     Person.find({name:body.name}).then(result => {
         if(result){
@@ -106,33 +106,34 @@ app.post('/api/persons', (request, response, next)=>{
         } else {
             person.save()
                 .then(savedPerson =>{
-                    response.json(savedPerson);
+                    response.json(savedPerson)
                 })
                 .catch(error=>next(error))
         }
-    }).catch(()=>next(error));
+    }).catch((error)=>next(error))
 
 })
 
 // update a number
 app.put('/api/persons/:id', (request,response,next)=>{
     // deconstruct body -> {name, number}
-    const body = request.body;
+    const body = request.body
 
     const person ={
         name: body.name,
         number: body.number
     }
 
-    Person.findByIdAndUpdate(
+    Person
+        .findByIdAndUpdate(
             request.params.id, 
             person, 
             {new: true, runValidators: true, context: 'query'}
         )
         .then(updatedPerson => {
-            response.json(updatedPerson);
+            response.json(updatedPerson)
         })
-        .catch(error => next(error));
+        .catch(error => next(error))
 
 })
 
--- models/person.js
+++ models/person.js
@@ -1,46 +1,46 @@
 // import mongoose
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
 // connect to mongodb
-const db_url = process.env.MONGODB_URI;
+const db_url = process.env.MONGODB_URI
 console.log(`connecting to ${db_url}`)
 
 mongoose
     .connect(db_url)
-    .then(result => {
-        console.log(`connected to MongoDB`);
+    .then(() => {
+        console.log('connected to MongoDB')
     })
     .catch((error)=>{
-        console.log(`error connecting to MongoDB: ${error.message}`);
-    });
+        console.log(`error connecting to MongoDB: ${error.message}`)
+    })
 
 const personSchema = new mongoose.Schema({
     name: {
-      type: String,
-      required: true,
-      minLength: 3
+        type: String,
+        required: true,
+        minLength: 3
     },
     number: {
-      type: String,
-      required: true,
-      minLength: 8,
-      validate: {
-        validator: v => {
-          const reg = new RegExp('^\\d{2,3}-\\d+$');
-          console.log(`${v}: ${reg.test(v)}`)
-          return reg.test(v)
-        },
-        message: props => `${props.value} is not a valid phone number!`
-      }
+        type: String,
+        required: true,
+        minLength: 8,
+        validate: {
+            validator: v => {
+                const reg = new RegExp('^\\d{2,3}-\\d+$')
+                console.log(`${v}: ${reg.test(v)}`)
+                return reg.test(v)
+            },
+            message: props => `${props.value} is not a valid phone number!`
+        }
     },
 })
 personSchema.set('toJSON', {
     transform: (document, returnedObject) => {
-      returnedObject.id = returnedObject._id.toString()
-      delete returnedObject._id
-      delete returnedObject.__v
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
     }
-  })
+})
 
 
-module.exports = mongoose.model('Person', personSchema);
+module.exports = mongoose.model('Person', personSchema)
